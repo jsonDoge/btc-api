e8e1fb263308d04528447532fa7dfd0831207dbb
Matas555 <matas.jasonas@outlook.com>
2021-09-12 13:52:11 +0300

    use axios to fetch/proxy price from source url

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
 require('dotenv').config()
 const express = require('express')
+const axios = require('axios')
 
 const { env } = process
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello!')
+})
+
+app.get('/api/v1/price', async (req, res) => {
+  const priceUrl = `${env.BTCUSD_SOURCE_URL}/price`
+
+  let response
+  try {
+    response = await axios.get(priceUrl)
+  } catch (e) {
+    return res.status(500).json({
+      error: {
+        message: 'Failed to fetch price',
+        reason: e.message,
+      },
+    })
+  }
+
+  const { price } = response?.data?.result || {}
+
+  res.json({ result: { price } })
 })
 
 app.listen(env.port, () => {
