3e8dbaa8d01e218570d14620fd6e72ef4c335571
Matas555 <matas.jasonas@outlook.com>
2021-09-12 17:11:59 +0300

    add hist endpoint

--- index.js
+++ index.js
@@ -30,6 +30,26 @@ app.get('/api/v1/price', async (req, res) => {
   res.json({ result: { price: rate } })
 })
 
+app.get('/api/v1/hist', async (req, res) => {
+  const histUrl = `${env.BTCUSD_SOURCE_URL}/ohlcv/COINBASE_SPOT_BTC_USD/latest?period_id=10MIN`
+
+  let response
+  try {
+    response = await axios.get(histUrl)
+  } catch (e) {
+    return res.status(500).json({
+      error: {
+        message: 'Failed to fetch hist',
+        reason: e.message,
+      },
+    })
+  }
+
+  const hist = response?.data
+
+  res.json({ result: { hist } })
+})
+
 app.listen(env.port, () => {
   console.log(`Listening on http://localhost:${env.port}`)
 })
